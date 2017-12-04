ef8fb612ee6276d6f5db2b18451d59700e9e6b48
chaopengz <chaopengz@pku.edu.cn>
2017-12-04 14:05:05 +0800

    update code in views.py

--- Server/web/views.py
+++ Server/web/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, render_to_response
 from django.http import *
 from django.template import loader, context, RequestContext
+from Furniture import *
 
 
 # Create your views here.
@@ -8,3 +9,13 @@ def index(request):
     return render_to_response("index.html")
 
 
+def receivePic(request):
+    if request.method == "POST":
+        # 接受图片，进行手势判断
+        pass
+    else:
+        return render_to_response("index.html")
+
+
+def getFurnitureState(request):
+    pass
