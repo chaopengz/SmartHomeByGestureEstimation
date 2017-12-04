89012e79fdfa3dc7f8b101fc85fda913def3869f
chaopengz <chaopengz@pku.edu.cn>
2017-12-04 10:41:18 +0800

    add four class: Furniture, Light, Soft, TV

--- Server/web/Furniture.py
+++ Server/web/Furniture.py
@@ -13,3 +13,27 @@ class Light(Furniture):
 
     def changeState(self):
         self.state = not self.state
+
+
+class soft(Furniture):
+    def __init__(self):
+        self.name = "soft"
+        self.state = False
+
+    def getState(self):
+        return self.state
+
+    def changeState(self):
+        self.state = not self.state
+
+
+class tv(Furniture):
+    def __init__(self):
+        self.name = "tv"
+        self.state = False
+
+    def getState(self):
+        return self.state
+
+    def changeState(self):
+        self.state = not self.state
